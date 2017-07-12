cf51005bcf0cf0852689c6a9810015c1524061bf
Codingsunshine <Jessica.baines@gmail.com>
2017-07-11 20:17:16 -0500

    Added first js
    added first javascript

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+/*Tell javascript to wait
+
+when window has finished loading do this*/
+
+window.onload= function(event) {
+	console.log('Hi mom');
+	console.log(document);
+	/*createElement('ul') means create a ul as a result*/
+	let ul = document.createElement('ul');
+	console.log(ul);
+
+document.getElementById('list').innerHTML = 'I like cats';
+
+
+
+
+
+}
